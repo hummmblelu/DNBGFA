80db6cfecb28767169f206fcaacf8376d4b55a11
hummmblelu <58004124+hummmblelu@users.noreply.github.com>
2021-06-07 03:01:19 +0300

    Create example_run.R

---
+++ example_run.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+
+## loading required functions
+source("DNBGFA_var_functions.R")
+
+## loading example data set (Contains objects X, and m_len)
+load("example_data.RData")
+
+## model inference 
+print("inference model")
+model<- model_setup(X, m_len, 20)
+update_model(model, iter = 1000, mc.cores = 10, print_topics = T)
+
+## model inference with interpolation (the second time stamp)
+print("inference model")
+X_remvd<- X[[2]]
+X[[2]]<- NULL ## make the second matrix null
+model<- model_setup(X, m_len, 20)
+model<- update_model_interp(model, iter = 10, inter_t = 2, mc.cores = 10)
+
+## compute expected Z at the time stamp 2
+exp_z<- apply(model$var_params$m_eta[,,2], 2, function(x) exp(x) / sum(exp(x)))
+
+## compute mean square error
+print(sqrt(mean((X_remvd - exp_z %*% t(model$var_params$exp_w[[2]]))^2)))
