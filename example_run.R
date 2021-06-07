ef3c833df7f3198a725e3493c8406c2c0760b7aa
hummmblelu <58004124+hummmblelu@users.noreply.github.com>
2021-06-07 11:49:03 +0300

    Update example_run.R

--- example_run.R
+++ example_run.R
@@ -14,7 +14,7 @@ update_model(model, iter = 1000, mc.cores = 10, print_topics = T)
 ## model inference with interpolation (the second time stamp)
 print("inference model")
 X_remvd<- X[[2]]
-X[[2]]<- NULL ## make the second matrix null
+X[[2]]<- matrix(NA, nrow(X[[2]]), ncol(X[[2]])) ## make the second matrix NA valued
 model<- model_setup(X, m_len, 20)
 model<- update_model_interp(model, iter = 1000, inter_t = 2, mc.cores = 10)
 
